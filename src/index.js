7f48552d17be97c3c745020ff50636457bca5737
Sammy Baek <whitesw01@gmail.com>
2022-02-19 14:21:18 +0900

    feat: 개선 아이디어 6 완료

--- src/index.js
+++ src/index.js
@@ -117,6 +117,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (this.state.stepNumber >= 9) {
+      status = 'Draw Game !';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
